437d367fbbbaa501a7b28a1c8b9851da3ed89514
Maria Lapshina <mariaspiiish@gmail.com>
2022-08-12 16:05:12 +0400

    fix: добавить недостающую константу

--- custom errors/error_status.js
+++ custom errors/error_status.js
@@ -1,8 +1,9 @@
 const ok = 200;
 const created = 201;
 const incorrectData = 400;
+const notFound = 404;
 const internalError = 500;
 
 module.exports = {
-  ok, created, incorrectData, internalError,
+  ok, created, incorrectData, internalError, notFound,
 };
