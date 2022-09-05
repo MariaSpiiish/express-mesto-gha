5c718b9ee0f7f0e109b782c0e542d22976900e57
Maria Lapshina <mariaspiiish@gmail.com>
2022-09-05 16:43:38 +0400

    feat: logger, port3010

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const authRouter = require('./routes/index');
@@ -8,7 +9,7 @@ const { auth } = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFound = require('./custom errors/NotFound');
 // Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const { PORT = 3010 } = process.env;
 
 const app = express();
 
@@ -18,9 +19,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use('/', authRouter);
-
 app.use(auth);
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
@@ -28,6 +29,8 @@ app.use('/', cardsRouter);
 app.use((req, res, next) => {
   next(new NotFound('404 Страница не найдена'));
 });
+
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
